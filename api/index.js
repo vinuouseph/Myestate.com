2d40dee2303b80e86aa1303e54d8ca22a3a031ed
Vinu Ouseph <128832117+vinuouseph@users.noreply.github.com>
2024-05-08 00:35:20 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@
     app.use(express.json());
     app.use(cookieParser());
     app.use(cors({
-        origin: 'http://localhost:5173'
+        origin: 'https://myestate.azurewebsites.net'
     }));
     
     const transporter = nodemailer.createTransport({
